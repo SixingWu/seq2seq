2038b470e0edf9cf169cf3e3d83bc936cdd7ae4f
mebiuw <wusixing@pku.edu.cn>
2017-12-17 20:07:47 +0800

    test for char-level

--- config/S2S/prepare.sh
+++ config/S2S/prepare.sh
@@ -8,4 +8,4 @@ data_dir=data/DisTest
 rm -rf ${data_dir}
 mkdir -p ${data_dir}
 
-prepare-data.py rawdata/DiaTest/huawei message response data/DiaTest  --no-tokenize --test-size 10000 --dev-size 10000 --vocab-size 30000 --shuffle --seed 1234
+prepare-stc-data.py rawdata/DiaTest/huawei message response data/DiaTest  --no-tokenize --test-size 10000 --dev-size 10000 --vocab-size 30000 --shuffle --seed 1234 --verbose --shared-vocab
