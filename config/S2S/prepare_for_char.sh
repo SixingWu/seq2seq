949c074dcac284d62c53f03369b98b1323b9d4c6
mebiuw <wusixing@pku.edu.cn>
2017-12-19 10:38:40 +0800

    Formal Char-level

--- config/S2S/prepare_for_char.sh
+++ config/S2S/prepare_for_char.sh
@@ -1,5 +1,9 @@
- python3 scripts/prepare-stc-data.py rawdata/DiaTest/huaweiFull2 message response data/DiaTestCharLevel  --no-tokenize --test-size 100000 --dev-size 100000 --vocab-size 50000 --shuffle --seed 1234 --verbose --shared-vocab --character-level
+ python3 scripts/prepare-stc-data.py rawdata/DiaTest/huaweiFull2 message response data/DiaTestCharLevel  --no-tokenize --test-size 450000 --dev-size 450000 --vocab-size 30000 --shuffle --seed 1234 --verbose --shared-vocab --character-level
  # train a baseline model on this data
  nohup ./seq2seq.sh config/S2S/charlevel.yaml --train -v --debug >> charlog3.txt &
 
  nohup ./seq2seq.sh config/S2S/charlevel.yaml --train -v --decode >> charlog.txt
+
+
+./seq2seq.sh config/S2S/charlevel.yaml -v --eval --gpu-id 1
+./seq2seq.sh config/S2S/charlevel.yaml -v --decode --gpu-id 1
